e6e6b6725500276a7223d77c87e95d1afd7c7a0a
Tom Alle <development@praetoros.com>
2024-05-09 01:29:27 +1000

    p506 - Better way of processing - less double handles

--- P506.cs
+++ P506.cs
@@ -5,44 +5,29 @@ public class P506
     public static void Run()
     {
         // Test Cases
-        Console.WriteLine(string.Join(",", FindRelativeRanks([5,4,3,2,1])));
-        Console.WriteLine(string.Join(",", FindRelativeRanks([10,3,8,9,4])));
+        Console.WriteLine(string.Join(",", FindRelativeRanks([5, 4, 3, 2, 1])));
+        Console.WriteLine(string.Join(",", FindRelativeRanks([10, 3, 8, 9, 4])));
     }
 
-    public static string[] FindRelativeRanks(int[] scores) {
-        var orderedScores = scores.OrderDescending();
-        var rankMap = new Dictionary<int, string>();
-        var ranks = new string[scores.Length];
-        if (scores.Length > 0)
+    public static string[] FindRelativeRanks(int[] scores)
+    {
+        var scoreMap = new Dictionary<int, int>();
+        for (int i = 0; i < scores.Length; i++)
         {
-            ranks[0] = "Gold Medal";
-        }
-        if (scores.Length > 1)
-        {
-            ranks[1] = "Silver Medal";
-        }
-        if (scores.Length > 2)
-        {
-            ranks[2] = "Bronze Medal";
-        }
-        for (int i = 3; i < scores.Length; i++)
-        {
-            ranks[i] = (i + 1).ToString();
+            scoreMap[scores[i]] = i;
         }
 
-        var index = 0;
-        foreach (var orderedScore in orderedScores)
+        Array.Sort(scores, (x, y) => y.CompareTo(x));
+        string[] result = new string[scores.Length];
+        for (int i = 0; i < scores.Length; i++)
         {
-            rankMap[orderedScore] = ranks[index];
-            index++;
-        }
-
-        var result = new string[scores.Length];
-        index = 0;
-        foreach (var score in scores)
-        {
-            result[index] = rankMap[score];
-            index++;
+            result[scoreMap[scores[i]]] = i switch
+            {
+                0 => "Gold Medal",
+                1 => "Silver Medal",
+                2 => "Bronze Medal",
+                _ => (i + 1).ToString()
+            };
         }
 
         return result;
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿using Problems;
 
+// Add the class of whatever problem you want to run
+
 P506.Run();
